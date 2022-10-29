dd2bfada7dbeffbef353bd05eec5b69308d39c1d
Jebbers7 <derinkaya52@gmail.com>
2022-10-29 11:25:03 -0700

    what i did on 10/29/22

---
+++ src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
@@ -0,0 +1,15 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+public static void main(String[] args) {
+	
+	String primenumber = JOptionPane.showInputDialog("Give me a number");
+	
+   
+	
+	
+	
+}
+}
