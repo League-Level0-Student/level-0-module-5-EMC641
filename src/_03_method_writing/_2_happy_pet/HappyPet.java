603a801d85a5fc9ae761629a9134547a6168cb0f
jeremycui2008 <54760319+OceanNut420@users.noreply.github.com>
2022-11-19 11:25:47 -0800

    other stuff I did on 11/19/22

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -14,31 +14,45 @@ public class HappyPet {
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "task", 0, JOptionPane.INFORMATION_MESSAGE, null,
+
+		int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "task", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Walk your pet", "Feed your pet", "Play catch with your pet?" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
 
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+		
 	
 
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
 	if(task==0) {
-	JOptionPane.showMessageDialog(null, " Your pet likes to be walked, but is tired right now. Maybe later she wants to be walked");
+	JOptionPane.showMessageDialog(null, " Your pet likes to be walked, but is tired right now. Maybe later she wants to be walked. You lost 5 happines points.");
 	task-=5;
 	}
 	else if(task==1) {
-		JOptionPane.showMessageDialog(null, " Your pet was very hungry.");
+		JOptionPane.showMessageDialog(null, " Your pet was very hungry. You earned 20 happiness points.");
 		task+=20;
 	}
 	else if(task==2) {
-		JOptionPane.showMessageDialog(null, " Your pet does't like to play catch.");
+		JOptionPane.showMessageDialog(null, " Your pet does't like to play catch.You lost 10 happines points.");
 		task-=10;
 	}
 	JOptionPane.showMessageDialog(null, task);
+	
+	
+	if(task==50) {
+		int something = JOptionPane.showMessageDialog(null, "You made your pet very happy. Do you want to play again?") "choice", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Yes", "No" }, null);
+		}
+		
+		if(task==1) {
+			
+		}
+		else if(task==2) {
+			System.exit(task);
+		}
 
 }}
